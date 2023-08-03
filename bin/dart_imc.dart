f7cc98a3bd64b081ff59aac7ac6b045295162e23
xdiegors <xdiegors@outlook.com>
2023-08-03 11:48:10 -0300

    Added try catch block

--- bin/dart_imc.dart
+++ bin/dart_imc.dart
@@ -5,20 +5,26 @@ import 'package:dart_imc/calcular_imc.dart';
 void main() {
   Pessoa pessoa = Pessoa();
 
-  print("Digite seu nome:");
-  String? nome = stdin.readLineSync();
-  pessoa.setNome(nome);
+  try {
+    print("Digite seu nome:");
+    String? nome = stdin.readLineSync();
+    pessoa.setNome(nome);
 
-  print("Digite seu peso:");
-  String? peso = stdin.readLineSync();
-  if (peso != null) {
-    pessoa.setPeso(double.tryParse(peso));
-  }
+    print("Digite seu peso:");
+    String? peso = stdin.readLineSync();
+    if (peso != null) {
+      pessoa.setPeso(double.tryParse(peso));
+    }
 
-  print("Digite sua altura:");
-  String? altura = stdin.readLineSync();
-  if (altura != null) {
-    pessoa.setAltura(double.tryParse(altura));
+    print("Digite sua altura:");
+    String? altura = stdin.readLineSync();
+    if (altura != null) {
+      pessoa.setAltura(double.tryParse(altura));
+    }
+  } catch (e) {
+    print(
+        "Verifique se os dados de entrada estão corretos. \nMensagem técnica: $e");
+    return;
   }
 
   print(
